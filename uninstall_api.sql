8df6c0fee7ef2fb1055988750fcad163e04a639a
Nikolay <github2@samokhvalov.com>
2017-04-26 14:55:20 -0700

    v2 schema instead of v1

--- uninstall_api.sql
+++ uninstall_api.sql
@@ -1,20 +1,20 @@
-revoke execute on function v1.google_translate(char, char, text) from apiuser;
-revoke execute on function v1.google_translate_array(char, char, json) from apiuser;
+revoke execute on function v2.google_translate(char, char, text) from apiuser;
+revoke execute on function v2.google_translate_array(char, char, json) from apiuser;
 
-revoke execute on function v1.promt_translate(char, char, text) from apiuser;
-revoke execute on function v1.promt_translate_array(char, char, json) from apiuser;
+revoke execute on function v2.promt_translate(char, char, text) from apiuser;
+revoke execute on function v2.promt_translate_array(char, char, json) from apiuser;
 
-revoke execute on function v1.bing_translate(char, char, text) from apiuser;
-revoke execute on function v1.bing_translate_array(char, char, json) from apiuser;
+revoke execute on function v2.bing_translate(char, char, text) from apiuser;
+revoke execute on function v2.bing_translate_array(char, char, json) from apiuser;
 
-drop function v1.google_translate(char, char, text);
-drop function v1.google_translate_array(char, char, json);
+drop function v2.google_translate(char, char, text);
+drop function v2.google_translate_array(char, char, json);
 
-drop function v1.promt_translate(char, char, text);
-drop function v1.promt_translate_array(char, char, json);
+drop function v2.promt_translate(char, char, text);
+drop function v2.promt_translate_array(char, char, json);
 
-drop function v1.bing_translate(char, char, text);
-drop function v1.bing_translate_array(char, char, json);
---revoke usage on schema v1 from apiuser;
---drop schema v1 cascade;
+drop function v2.bing_translate(char, char, text);
+drop function v2.bing_translate_array(char, char, json);
+--revoke usage on schema v2 from apiuser;
+--drop schema v2 cascade;
 --drop role apiuser;
