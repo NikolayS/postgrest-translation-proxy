7a4fc17988f4ef9c180ffe639b304b80bb6afc12
Dimytch <dimytch@a.toh.info>
2017-04-28 22:57:28 +0300

    api, first approach

--- install_api.sql
+++ install_api.sql
@@ -1,15 +1,31 @@
-create schema if not exists v2;
-do
+alter database DBNAME set translation_proxy.api.current_engine = 'CURRENT_API_ENGINE'
+
+CREATE SCHEMA IF NOT EXISTS 'API_SCHEMA_NAME';
+DO
 $$
-begin
-   if not exists (
-      select *
-      from   pg_catalog.pg_user
-      where  username = 'apiuser'
-    ) then
-      create role apiuser password 'pass-for-apiuser' login;
-   end if;
-end
+BEGIN
+   IF NOT EXISTS (
+      SELECT *
+      FROM pg_catalog.pg_user
+      WHERE  usename = 'API_USERNAME'
+    ) THEN
+      CREATE ROLE API_USERNAME PASSWORD 'APIUSER-PASSWORD' LOGIN;
+   END IF;
+END
 $$;
 
-grant usage on schema v2 to apiuser;
+GRANT USAGE ON SCHEMA 'API_SCHEMA_NAME' TO API_USERNAME;
+
+CREATE OR REPLACE FUNCTION API_SCHEMA_NAME.translate_array(source CHAR(2), target CHAR(2), q JSON)
+RETURNS TEXT[] AS $$
+  CASE current_setting('translation_proxy.api.current_engine')
+    WHEN 'google' THEN
+      SELECT * FROM translation_proxy.google_translate_array( source, target, q );
+    WHEN 'promt' THEN
+      SELECT * FROM translation_proxy.promt_translate_array( source, target, q );
+    WHEN 'bing' THEN
+      SELECT * FROM translation_proxy.bing_translate_array( source, target, q );
+  END CASE;
+$$ LANGUAGE SQL SECURITY DEFINER;
+
+GRANT EXECUTE ON FUNCTION API_SCHEMA_NAME.translate_array(char, char, json) TO API_USERNAME;
--- setup.rb
+++ setup.rb
@@ -15,54 +15,67 @@ end
 
 require 'yaml'
 class Setup
-  def setup
-    @cfg = YAML.load_file 'setup.yml'
+  @@cfg = YAML.load_file 'setup.yml'
+  def setup_core
     # Global setup
     [:global, :google, :promt, :bing].each do |engine|
       filename = "install_#{ engine }_core.sql"
-      @cfg[engine][:script] = File.read(filename)
-        .gsub( ' DBNAME ', " #{ @cfg[:global][:database] } ")
+      @@cfg[engine][:script] = File.read(filename)
+        .gsub( ' DBNAME ', " #{ @@cfg[:global][:database] } ")
     end
-    ENV['PGPASSWORD'] = @cfg[:global][:password] unless @cfg[:global][:password].nil?
-    ENV['PGUSER'] = @cfg[:global][:username] unless @cfg[:global][:username].nil?
+    ENV['PGPASSWORD'] = @@cfg[:global][:password] unless @@cfg[:global][:password].nil?
+    ENV['PGUSER'] = @@cfg[:global][:username] unless @@cfg[:global][:username].nil?
     # @psql = File.open '/tmp/setup-script.sql', 'w'
     [:global, :google, :promt, :bing].each do |engine|
-      @psql = IO::popen( [ 'psql', @cfg[:global][:database] ], 'w' )
-      self.send( "setup_#{ engine }" ) if @cfg[engine][:use]
+      @psql = IO::popen( [ 'psql', @@cfg[:global][:database] ], 'w' )
+      self.send( "setup_#{ engine }" ) if @@cfg[engine][:use]
       @psql.close
     end
+    self
+  end
 
+  def setup_api
+    api_code = File.read('install_api.sql')
+    api_code.gsub!
+    api_code.gsub!
+    @psql = IO::popen( [ 'psql', @@cfg[:global][:database] ], 'w' )
+    @psql.write api_code.gsub( /APIUSER-PASSWORD/, @@cfg[:api][:password] )
+        .gsub( /API_SCHEMA_NAME/, @@cfg[:api][:schema] )
+        .gsub( /API_USERNAME/, @@cfg[:api][:user] )
+        .gsub( /CURRENT_API_ENGINE/, @@cfg[:api][:current_engine] )
+    @psql.close
+    self
   end
 
   def setup_global
     puts "\t\t ==== Core features"
-    @psql.write @cfg[:global][:script] + "\n"
+    @psql.write @@cfg[:global][:script] + "\n"
   end
 
   def setup_google
     puts "\t\t ==== Setup Google API"
-    @psql.write @cfg[:google][:script].gsub( /YOUR_GOOGLE_API_KEY/, @cfg[:google][:api_key] )
-                .gsub( /GOOGLE_BEGIN_AT/, @cfg[:google][:begin_at].to_s )
-                .gsub( /GOOGLE_END_AT/, @cfg[:google][:end_at].to_s ) + "\n"
+    @psql.write @@cfg[:google][:script].gsub( /YOUR_GOOGLE_API_KEY/, @@cfg[:google][:api_key] )
+                .gsub( /GOOGLE_BEGIN_AT/, @@cfg[:google][:begin_at].to_s )
+                .gsub( /GOOGLE_END_AT/, @@cfg[:google][:end_at].to_s ) + "\n"
   end
 
   def setup_promt
     puts "\t\t ==== Setup Promt API"
-    @psql.write @cfg[:promt][:script].gsub(/YOUR_PROMT_LOGIN/, @cfg[:promt][:login] )
-                  .gsub( /YOUR_PROMT_PASSWORD/, @cfg[:promt][:password] )
-                  .gsub( /YOUR_PROMT_SERVER_URL/, @cfg[:promt][:server_url] )
-                  .gsub( /PROMT_LOGIN_TIMEOUT/, @cfg[:promt][:login_timeout] )
-                  .gsub( /PROMT_COOKIE_FILE/, @cfg[:promt][:cookie_file])
-                  .gsub( /PROMT_KEY_VALID_FROM/, @cfg[:promt][:valid_from].to_s )
-                  .gsub( /PROMT_KEY_VALID_UNTIL/, @cfg[:promt][:valid_until].to_s ) + "\n"
+    @psql.write @@cfg[:promt][:script].gsub(/YOUR_PROMT_LOGIN/, @@cfg[:promt][:login] )
+                  .gsub( /YOUR_PROMT_PASSWORD/, @@cfg[:promt][:password] )
+                  .gsub( /YOUR_PROMT_SERVER_URL/, @@cfg[:promt][:server_url] )
+                  .gsub( /PROMT_LOGIN_TIMEOUT/, @@cfg[:promt][:login_timeout] )
+                  .gsub( /PROMT_COOKIE_FILE/, @@cfg[:promt][:cookie_file])
+                  .gsub( /PROMT_KEY_VALID_FROM/, @@cfg[:promt][:valid_from].to_s )
+                  .gsub( /PROMT_KEY_VALID_UNTIL/, @@cfg[:promt][:valid_until].to_s ) + "\n"
   end
 
   def setup_bing
     puts "\t\t ==== Setup MS Bing API"
-    @psql.write @cfg[:bing][:script].gsub( /YOUR_BING_API_KEY/, @cfg[:bing][:api_key] )
-                  .gsub( /BING_TOKEN_EXPIRATION/, @cfg[:bing][:token_expiration] ) + "\n"
+    @psql.write @@cfg[:bing][:script].gsub( /YOUR_BING_API_KEY/, @@cfg[:bing][:api_key] )
+                  .gsub( /BING_TOKEN_EXPIRATION/, @@cfg[:bing][:token_expiration] ) + "\n"
   end
 
 end
 
-Setup.new.setup
+Setup.new.setup_core.setup_api
